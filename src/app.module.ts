29a2df09faff19b46ef9f9d6cc7aaae52d8f5c34
bii <kevinkipkoech@kabarak.ac.ke>
2023-03-11 13:31:58 +0300

    Initial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { MinioModule } from './minio/minio.module';
+import { AppController } from '/app.controller';
+import { AppService } from '/app.service';
+import { MinioModule } from '/minio.module';
 
 @Module({
   imports: [MinioModule],
